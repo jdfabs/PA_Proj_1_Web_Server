040dead5018a6fe926271002d5c295fca47dbb61
jdfabs <jdfabs1@gmail.com>
2025-03-19 18:52:34 +0000

    test: making TestLogger artificilly slower to log

--- src/test/java/TestLogger.java
+++ src/test/java/TestLogger.java
@@ -1,5 +1,7 @@
 import logging.Logger;
 
+import java.util.concurrent.TimeUnit;
+
 public class TestLogger extends Logger {
     //Mockito spy(Logger) did not allow method calls inside the spyed class, created this class to test multiple calls
     private int infoCallCount = 0;
@@ -11,6 +13,11 @@ public class TestLogger extends Logger {
     @Override
     public void info(String message) {
         infoCallCount++;
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         super.info(message);
     }
 
