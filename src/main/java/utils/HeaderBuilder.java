4354953058f7b5cb52555f41a3d0eafe787c6137
jdfabs <jdfabs1@gmail.com>
2025-03-27 20:25:22 +0000

    docs: HeaderBuilder

--- src/main/java/utils/HeaderBuilder.java
+++ src/main/java/utils/HeaderBuilder.java
@@ -5,9 +5,27 @@ import logging.LogProducer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * A thread that builds standard HTTP response headers.
+ * <p>
+ * The {@code HeaderBuilder} prepares static headers such as {@code Content-Type}, {@code Server},
+ * and the current {@code Date}, following basic HTTP formatting.
+ * The result is stored internally and can be retrieved using {@link #getHeader()}.
+ */
 public class HeaderBuilder extends Thread implements LogProducer {
+    /** A builder used to construct the HTTP response headers. */
     private final StringBuilder headerBuilder = new StringBuilder();
 
+    /**
+     * Constructs the HTTP headers when the thread is executed.
+     * <p>
+     * This includes:
+     * <ul>
+     *     <li>{@code Content-Type: text/html}</li>
+     *     <li>{@code Server: pa-web-server}</li>
+     *     <li>{@code Date: <current date>}</li>
+     * </ul>
+     */
     public void run() {
         headerBuilder.append("Content-Type: text/html\r\n");
         headerBuilder.append("Server: pa-web-server\r\n");
@@ -16,6 +34,11 @@ public class HeaderBuilder extends Thread implements LogProducer {
         headerBuilder.append("\r\n");
     }
 
+    /**
+     * Returns the constructed HTTP headers as a string.
+     *
+     * @return the formatted HTTP header block
+     */
     public String getHeader() {
         return headerBuilder.toString();
     }
