e5fa1d1dff3983ad412fecf873fd01c64904e699
jdfabs <jdfabs1@gmail.com>
2025-03-27 19:30:48 +0000

    docs: ThreadPool doc cleanup

--- src/main/java/core/ThreadPool.java
+++ src/main/java/core/ThreadPool.java
@@ -6,23 +6,28 @@ import java.util.concurrent.LinkedBlockingQueue;
 /**
  * A thread pool that manages a fixed number of worker threads to execute tasks.
  * <p>
- * This class initializes a pool of {@link WorkerThread} instances that process tasks
- * submitted to a shared {@link BlockingQueue}. Tasks are added via the
- * {@link #execute(Runnable)} method and executed by available worker threads. The pool
- * can be shut down using the {@link #shutdown()} method, after which no new tasks are accepted.
+ * This class initializes a pool of {@link WorkerThread} instances that continuously
+ * fetch and execute {@link Runnable} tasks from a shared {@link BlockingQueue}.
+ * Tasks are submitted using the {@link #execute(Runnable)} method.
+ * </p>
+ * <p>
+ * The thread pool can be gracefully shut down using {@link #shutdown()}, after which
+ * no new tasks will be accepted and all threads will be interrupted.
  * </p>
  */
 public class ThreadPool {
-    private final int poolSize;
+    /** Array of worker threads that execute submitted tasks. */
     private final WorkerThread[] workers;
+    /** A blocking queue that holds tasks to be processed by worker threads. */
     private final BlockingQueue<Runnable> taskQueue;
+    /** Indicates whether the thread pool has been shut down. */
     private volatile boolean isShutdown = false;
 
     /**
-     * Creates a thread pool with the specified number of worker threads.
+     * Constructs a thread pool with the specified number of worker threads.
      * <p>
-     * Initializes a {@link LinkedBlockingQueue} for task storage and starts the specified
-     * number of {@link WorkerThread} instances to process tasks from the queue.
+     * A {@link LinkedBlockingQueue} is used for storing tasks, and each worker
+     * thread is started immediately upon creation.
      * </p>
      *
      * @param poolSize the number of worker threads in the pool
@@ -40,12 +45,12 @@ public class ThreadPool {
     /**
      * Submits a task to the thread pool for execution.
      * <p>
-     * If the pool is not shut down, the task is added to the task queue. If an
-     * {@link InterruptedException} occurs while adding the task, the thread's interrupt
-     * status is restored.
+     * If the thread pool has not been shut down, the task is added to the task queue
+     * and will be picked up by an available worker thread. If the pool is shut down,
+     * the task will be ignored.
      * </p>
      *
-     * @param task the task to be executed by a worker thread
+     * @param task the {@link Runnable} task to be executed
      */
     public void execute(Runnable task) {
         if (!isShutdown) {
@@ -58,10 +63,10 @@ public class ThreadPool {
     }
 
     /**
-     * Shuts down the thread pool, interrupting all worker threads.
+     * Shuts down the thread pool, preventing any new tasks from being submitted.
      * <p>
-     * After this method is called, no new tasks can be submitted, and all worker threads
-     * are interrupted to stop task processing. The shutdown flag is set to <code>true</code>.
+     * All currently running worker threads will be interrupted, and the {@code isShutdown}
+     * flag is set to {@code true}.
      * </p>
      */
     public void shutdown() {
@@ -72,9 +77,9 @@ public class ThreadPool {
     }
 
     /**
-     * Returns the number of worker threads in the pool.
+     * Returns the array of worker threads currently managed by this thread pool.
      *
-     * @return the pool size
+     * @return an array of {@link WorkerThread} instances
      */
     public WorkerThread[] getWorkers() {
         return workers;
