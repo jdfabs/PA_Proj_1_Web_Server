873f58ca82421036e081cc59101aa4afc2e2cf4c
jdfabs <jdfabs1@gmail.com>
2025-03-27 19:57:24 +0000

    docs: Logging Enums

--- src/main/java/logging/LogLocation.java
+++ src/main/java/logging/LogLocation.java
@@ -1,5 +1,10 @@
 package logging;
 
+/**
+ * Represents the output destination for a log message.
+ * <p>
+ * Used by the {@link logging.Logger} to determine where to write the log.
+ */
 public enum LogLocation {
     ConsoleOut,
     ConsoleErr,
--- src/main/java/logging/LogType.java
+++ src/main/java/logging/LogType.java
@@ -1,5 +1,10 @@
 package logging;
 
+/**
+ * Represents the severity or category of a log message.
+ * <p>
+ * Used by the {@link logging.LoggingTask} to determine how the message should be handled and formatted.
+ */
 public enum LogType {
     Warning,
     Error,
