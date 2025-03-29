cf8723e7d88b3da7acf39cffdb845257fd89f704
jdfabs <jdfabs1@gmail.com>
2025-03-29 22:32:15 +0000

    test: ServerConfigTest Extension

--- src/test/java/ServerConfigTest.java
+++ src/test/java/ServerConfigTest.java
@@ -1,52 +1,62 @@
 import config.ServerConfig;
-import logging.LogLocation;
-import logging.LogType;
-import logging.LoggingTask;
-import logging.SharedBuffer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServerConfigTest {
 
     private ServerConfig config;
+    private ServerConfig emptyConfig;
 
     @BeforeEach
     void setUp() {
         // Load config file for tests (from src/test/resources)
         config = new ServerConfig("src/test/java/resources/server.config");
+        emptyConfig = new ServerConfig("");
     }
 
     @Test
     void testGetRoot() {
         assertEquals(System.getProperty("user.dir")+"/test_html/", config.getRoot());
+        assertEquals("", emptyConfig.getRoot());
     }
 
     @Test
     void testGetPort() {
         assertEquals(9090, config.getPort());
+        assertEquals(8080, emptyConfig.getPort());
     }
 
     @Test
     void testGetDefaultPage() {
         assertEquals("index", config.getDefaultPageFile());
+        assertEquals("index", emptyConfig.getDefaultPageFile());
     }
 
     @Test
     void testGetDefaultPageExtension() {
         assertEquals("html", config.getDefaultPageExtension());
+        assertEquals("html", emptyConfig.getDefaultPageExtension());
     }
 
     @Test
     void testGetPage404() {
         assertEquals("test_404.html", config.getPage404());
+        assertEquals("404.html", emptyConfig.getPage404());
     }
 
     @Test
     void testGetMaximumRequests() {
         assertEquals(10, config.getMaxRequests());
+        assertEquals(5, emptyConfig.getMaxRequests());
+    }
+
+    @Test
+    void testGetCacheExpiration() {
+        assertEquals(Duration.ofSeconds(10),config.getCacheExpirationTime());
+        assertEquals(Duration.ofSeconds(30),emptyConfig.getCacheExpirationTime());
     }
 }
