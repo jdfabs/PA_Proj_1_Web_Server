dfa73df2108bab3ebd7c0beae8b0dc5a85d56ffe
jdfabs <jdfabs1@gmail.com>
2025-03-29 22:59:12 +0000

    test: LoggerTest fix

--- src/test/java/LoggerTest.java
+++ src/test/java/LoggerTest.java
@@ -100,15 +100,13 @@ public class LoggerTest {
     public void testLoggingTaskGetters() {
         LocalDateTime before = LocalDateTime.now();
         LoggingTask task = new LoggingTask(LogType.Info, LogLocation.ConsoleOut, "Task message");
-        LocalDateTime after = LocalDateTime.now();
-
         assertEquals(LogType.Info, task.getType());
         assertEquals(LogLocation.ConsoleOut, task.getLocation());
         assertEquals("Task message", task.getMessage());
 
 
 
-        assertTrue(task.getRequestTime().isAfter(before) && task.getRequestTime().isBefore(after));
+        assertTrue(task.getRequestTime().isAfter(before) && task.getRequestTime().isBefore(LocalDateTime.now()));
     }
 
     @Test
