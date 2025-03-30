a3036dad12a0ea13c0e3788bb0d80737ddf13343
GoncasFr <goncasfr@gmail.com>
2025-03-30 16:29:09 +0100

    test: RequestHandlerTest now has a new method "shouldRespond200WhenFileExists" thus increasing coverage to 6/7 methods.

--- src/test/java/RequestHandlerTest.java
+++ src/test/java/RequestHandlerTest.java
@@ -43,4 +43,33 @@ class RequestHandlerTest {
         assertTrue(response.contains("404 Not Found"));
         assertTrue(response.contains("404"));
     }
+
+    @Test
+    void shouldRespond200WhenFileExists() throws Exception {
+        // Arrange
+        String httpRequest = "GET /index.html HTTP/1.1\r\n\r\n";
+
+        BufferedReader input = new BufferedReader(new StringReader(httpRequest));
+        RequestHandler handler = new RequestHandler(input, clientOutput, config, "127.0.0.1");
+
+        // Act
+        handler.processRequest();
+
+        // Assert
+        String response = clientOutput.toString();
+        assertTrue(response.contains("HTTP/1.1 200 OK"), "Message must contain status 200 OK");
+        assertTrue(response.contains("Content-Type: text/html"), "Message must contain header Content-Type");
+        assertTrue(response.contains("Server: pa-web-server"), "Message must contain header Server");
+        assertTrue(response.contains("Date:"), "Message must contain header Date");
+        assertTrue(response.contains("<!DOCTYPE html>"), "Message must contain DOCTYPE html");
+        assertTrue(response.contains("<html lang=\"en-GB\">"), "Message must contain html lang en-GB");
+        assertTrue(response.contains("<head>"), "Message must contain head");
+        assertTrue(response.contains("<title>HOME</title>"), "Message must contain title HOME");
+        assertTrue(response.contains("</head>"), "Message must contain /head");
+        assertTrue(response.contains("<body>"), "Message must contain body");
+        assertTrue(response.contains("<h1>Welcome to PA Website</h1>"), "Message must contain Welcome to PA Website");
+        assertTrue(response.contains("<p>This is the index.html file</p>"), "Message must contain This is the index.html file");
+        assertTrue(response.contains("</body>"), "Message must contain /body");
+        assertTrue(response.contains("</html>"), "Message must contain /html");
+    }
 }
